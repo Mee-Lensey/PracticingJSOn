fa5034a158dba9b4c1935371ce7deaa3134f88a8
Mee-Lensey <148074806+Mee-Lensey@users.noreply.github.com>
2025-11-20 11:08:12 -0500

    console

--- index.js
+++ index.js
@@ -11,21 +11,40 @@ const dataArray = data.filter(book=>{
 const cleanArray = dataArray.filter(book=>{
 
     return book['title']!=="";
-})
+});
 
 
 
-console.log(dataArray)
-console.log(cleanArray)
-cleanArray.forEach(book=>{
 
-   console.log(book.title);
-}
+const readingBooks = cleanArray.filter(book => { // book that were reads 
+    return book['read?'] !== "No";
+});
 
-);
+const read
+
+
+
+// calculate how many days it took to read all of the books then divided them by how many books were read
+
+
+
+
+//const averagepages= pageCounts.reduce(((a, b) => a + b, 0) / pageCounts.length) // making an attempt for the avg
+console.log(readingBooks)
+// console.log(dataArray)
+// console.log(cleanArray)
+// cleanArray.forEach(book=>{
+
+//    console.log(book.title);
+// }
+
+//);
 // fetch('./rawBooks.json')
 //     .then((response) => response.json())
 //     .then((json) => console.log(json));
+
+
+// 1
 
 
 
