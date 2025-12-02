ff54c878ce2db5a1d82d8ece011b9ec13ed23372
Mee-Lensey <148074806+Mee-Lensey@users.noreply.github.com>
2025-12-02 11:24:30 -0500

    525

--- index.js
+++ index.js
@@ -16,15 +16,43 @@ const cleanArray = dataArray.filter(book=>{
 
 
 
-const readingBooks = cleanArray.filter(book => { // book that were reads 
-    return book['read?'] !== "No";
+const readBooks = cleanArray.filter(book => { // book that were reads 
+    return book['read?'] !== "No" && (book["Time on TBR (days)"]>36525);  // come back to this = why cant i print the books that have  less than 36525 for [Time on TBR Days]?
 });
 
-const read
+let readingtime= 0;
+
+    readBooks.forEach(book=>{
+
+                console.log(book["read?"])
+
+        
+                 
+        console.log(book["Time on TBR (days)"]>36525)
+
+
+
+    });
+
+
+[cleanArray.filter];
+
+
+//document.getElementById("demo").innerHTML = numbers.reduce(getSum,0);
+
+function getSum(total,num) {
+    
+    return total+Math.round(num);
+}
+
+
+
+
 
 
 
 // calculate how many days it took to read all of the books then divided them by how many books were read
+
 
 
 
